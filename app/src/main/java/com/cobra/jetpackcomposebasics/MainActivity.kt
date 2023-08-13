7eb0749a7cd078c7ee3f97c485705bc1168614f3
mahhmoud refaat <mahhmoudrefat@outlook.com>
2023-08-14 00:40:11 +0300

    calling functions test

--- app/src/main/java/com/cobra/jetpackcomposebasics/MainActivity.kt
+++ app/src/main/java/com/cobra/jetpackcomposebasics/MainActivity.kt
@@ -3,41 +3,30 @@ package com.cobra.jetpackcomposebasics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.cobra.jetpackcomposebasics.ui.theme.JetpackComposeBasicsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeBasicsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+           // بنادى هنا عشان تتنفذ فى الابب
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    JetpackComposeBasicsTheme {
-        Greeting("Android")
-    }
-}
---
+++ app/src/main/java/com/cobra/jetpackcomposebasics/code examples.kt
@@ -0,0 +1,31 @@
+package com.cobra.jetpackcomposebasics
+
+class `Code_examples` {
+    /*
+    امثلة للموديفير الى ممكن نكتبها
+     @Composable
+fun FirstApp() {
+    Surface(
+        modifier = Modifier
+            .height(150.dp)
+            .width(250.dp).padding(20.dp),
+        color = Color.Green,
+        shape = CutCornerShape(16.dp),
+        border = BorderStroke(width = 1.dp, color = Color.DarkGray),
+        elevation = 20.dp
+    ) {
+        Text(text = "Hello world !!", modifier = Modifier.padding(16.dp))
+    }
+}
+     */
+/*
+
+//this function preview to show the ui in panel
+//عشان كدة نادينا على الفانكشن بتاع الفيرست ابب غبها
+@Preview(name = "THis is preview", showBackground = true)
+@Composable
+fun AppPreview() {
+    FirstApp()
+}
+ */
+}
