a53e5d82b849f3d2fd1d43270df211f3ccdd29ae
mahhmoud refaat <mahhmoudrefat@outlook.com>
2023-08-14 23:09:00 +0300

    calling functions test2

--- app/src/main/java/com/cobra/jetpackcomposebasics/MainActivity.kt
+++ app/src/main/java/com/cobra/jetpackcomposebasics/MainActivity.kt
@@ -4,16 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -24,9 +23,35 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-           // بنادى هنا عشان تتنفذ فى الابب
+            // بنادى هنا عشان تتنفذ فى الابب
+            MyApp()
+
         }
     }
 }
 
+@Composable
+fun MyApp() {
+    Row(
+        modifier = Modifier
+            .width(200.dp)
+            .height(150.dp)
+            .fillMaxSize()
+            .background(Color.Cyan),
+        verticalAlignment = Alignment.CenterVertically
+        , horizontalArrangement = Arrangement.SpaceAround
+    ) {
+        Text(text = "Hello")
+        Text(text = "android")
+        Text(text = "android")
+        Text(text = "android")
+    }
 
+}
+
+@Preview
+@Composable
+fun AppPreview() {
+    MyApp()
+
+}
